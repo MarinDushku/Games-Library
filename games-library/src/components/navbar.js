70552906643c830d587e1e55aa5d4e4f07980e7b
Marin D <dushkumarini@gmail.com>
2025-06-29 16:05:47 -0400

    Working Navbar

--- games-library/src/components/navbar.js
+++ games-library/src/components/navbar.js
@@ -1,3 +1,22 @@
-export default function navbar(){
-  return <div>Hidnkdjsahkfhksdkfskfs</div>;
+import Logo from '../images/logo.png';
+import './navbar.css';
+
+export default function Navbar(){  
+  return (
+    <div className="navbar">
+      <img src={Logo} alt="Logo" className="navbar-logo" />  
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Store</li>
+        <li>About</li>
+        <li>Support</li>
+        <div className="search-container">
+          <input type="text" placeholder="Search..." />
+          <button type="button" className="search-button">
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
+      </ul>
+    </div>
+  );
 }
