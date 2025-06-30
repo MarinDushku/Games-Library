dc6ce03138612f56efce943017c0fa1c3567efd9
NevinGuni <nevinguni@yahoo.com>
2025-06-30 12:54:41 +0200

    library start

--- games-library/src/components/library/library.js
+++ games-library/src/components/library/library.js
@@ -1,7 +1,26 @@
+import background from './background library2.jpg'; // Two ../ to go up from components/library to src
+import './library.css'; 
 export default function Library() {
   return (
-      <div>
-      <p>This is the library component</p>
+    <div className="library-container" style={{backgroundImage: `url(${background})`}}>
+      <aside className="sidebar">
+        <h2>Categories</h2>
+        <ul>
+          <li>Action</li>
+          <li>Adventure</li>
+          <li>Arcade</li>
+          <li>Fighting</li>
+          <li>Horror</li>
+          <li>Puzzle</li>
+          <li>Sports</li>
+          <li>Strategy</li>
+          <li>RPG</li>
+        </ul>
+      </aside>
+      
+      <main className="main-content">
+        <h1>Game Library</h1>
+      </main>
     </div>
   );
 }
