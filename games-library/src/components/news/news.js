02031810c74b5b201b3e43c49f5d52f07daa949f
NevinGuni <nevinguni@yahoo.com>
2025-07-01 16:24:24 +0200

    starting news page

---
+++ games-library/src/components/news/news.js
@@ -0,0 +1,10 @@
+import React, { useState, useRef } from 'react';
+import { newsData } from './newsdata.js';
+import './news.css';
+
+export default function News() {
+
+    const [hoveredNews, setHoveredNews] = useState(null);
+    const [showPopup, setShowPopup] = useState(false);
+   
+};
